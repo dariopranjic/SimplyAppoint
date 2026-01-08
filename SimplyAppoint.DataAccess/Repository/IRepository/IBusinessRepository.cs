c45693a81437d81f4ec9d96c58da5acbe6bad0d9
Dario Pranjić <dario.pranjic08@gmail.com>
2026-01-08 19:52:29 +0100

    Added IBusinessRepository

---
+++ SimplyAppoint.DataAccess/Repository/IRepository/IBusinessRepository.cs
@@ -0,0 +1,13 @@
+﻿using SimplyAppoint.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimplyAppoint.DataAccess.Repository.IRepository
+{
+    public interface IBusinessRepository : IRepository<Business>
+    {
+        void Update(Business obj);
+        void Save();
+    }
+}
