be68ae403ac7c809e1d34ff09d5dfd1cc287907f
Dario Pranjić <dario.pranjic08@gmail.com>
2026-01-08 19:37:51 +0100

    Added TimeOff model

---
+++ SimplyAppoint.Models/TimeOff.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SimplyAppoint.Models
+{
+    [Index(nameof(BusinessId))]
+    [Index(nameof(BusinessId), nameof(StartUtc))]
+    public class TimeOff
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int BusinessId { get; set; }
+
+        [Required]
+        public DateTimeOffset StartUtc { get; set; }
+
+        [Required]
+        public DateTimeOffset EndUtc { get; set; }
+
+        [MaxLength(200)]
+        public string? Reason { get; set; }
+
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; } = DateTimeOffset.UtcNow;
+    }
+}
