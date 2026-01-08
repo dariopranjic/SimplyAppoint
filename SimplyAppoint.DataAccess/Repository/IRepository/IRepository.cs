630c2e056694ff4436d3dcfe8a2499845b47bc6d
Dario Pranjić <dario.pranjic08@gmail.com>
2026-01-08 13:49:35 +0100

    Added IRepository interface

---
+++ SimplyAppoint.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace SimplyAppoint.DataAccess.Repository.IRepository
+{
+    internal interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entity);
+    }
+}
