0ec28f92d13f22b8509f63341607894ee6b371e4
Dario Pranjić <dario.pranjic08@gmail.com>
2026-01-08 17:37:31 +0100

    Added Service model and made some updates in the Business model

--- SimplyAppoint.Models/Business.cs
+++ SimplyAppoint.Models/Business.cs
@@ -8,30 +8,37 @@ using System.Threading;
 namespace SimplyAppoint.Models
 {
     [Index(nameof(Slug), IsUnique = true)]
+    [Index(nameof(OwnerUserId))]
     public class Business
     {
         [Key]
         public int Id { get; set; }
-        [Required]
-        [MaxLength(450)]
-        public string OwnerUserId { get; set; }
-        [Required]
-        [MaxLength(200)]
-        public string Name { get; set; }
-        [Required]
-        [MaxLength(120)]
-        public string Slug { get; set; }
-        [Required]
-        [MaxLength(100)]
-        public string TimeZoneId { get; set; }
-        [Required]
-        [MaxLength(50)]
-        public string Phone { get; set; }
-        [Required]
-        [MaxLength(300)]
-        public string Address { get; set; }
-        [Required]
-        public DateTimeOffset CreatedUtc { get; set; }
 
+        [Required(AllowEmptyStrings = false)]
+        [MaxLength(450)]
+        public string OwnerUserId { get; set; } = default!;
+
+        [Required(AllowEmptyStrings = false)]
+        [MaxLength(200)]
+        public string Name { get; set; } = default!;
+
+        [Required(AllowEmptyStrings = false)]
+        [MaxLength(120)]
+        public string Slug { get; set; } = default!;
+
+        [Required(AllowEmptyStrings = false)]
+        [MaxLength(100)]
+        public string TimeZoneId { get; set; } = default!;
+
+        [Required(AllowEmptyStrings = false)]
+        [MaxLength(50)]
+        public string Phone { get; set; } = default!;
+
+        [Required(AllowEmptyStrings = false)]
+        [MaxLength(300)]
+        public string Address { get; set; } = default!;
+
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; } = DateTimeOffset.UtcNow;
     }
 }
---
+++ SimplyAppoint.Models/Service.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SimplyAppoint.Models
+{
+    [Index(nameof(BusinessId))]
+    [Index(nameof(BusinessId), nameof(Name), IsUnique = true)]
+    public class Service
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int BusinessId { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        [MaxLength(200)]
+        public string Name { get; set; } = default!;
+
+        [Required]
+        [Range(1, 24 * 60)]
+        public int DurationMinutes { get; set; }
+
+        [Required]
+        [Precision(10, 2)]
+        [Range(0, 1000000)]
+        public decimal Price { get; set; }
+
+        [Range(0, 24 * 60)]
+        public int BufferBefore { get; set; } = 0;
+
+        [Range(0, 24 * 60)]
+        public int BufferAfter { get; set; } = 0;
+
+        [Required]
+        public bool IsActive { get; set; } = true;
+
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; } = DateTimeOffset.UtcNow;
+    }
+}
