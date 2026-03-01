5d8569a411b842f5693b83e06fc3795cf5a6ad0c
dariopranjic <224952707+dariopranjic@users.noreply.github.com>
2026-03-01 21:39:32 +0100

    Created Services folder in the ViewModel folder also created ServiceUpsertVM and ServicesIndexVM

---
+++ SimplyAppoint.Models/ViewModels/Services/ServiceUpsertVM.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SimplyAppoint.Models.ViewModels
+{
+    public class ServiceUpsertVM
+    {
+        public int? Id { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Name { get; set; } = "";
+
+        [Required]
+        [Range(1, 24 * 60)]
+        public int DurationMinutes { get; set; }
+
+        [Required]
+        [Range(0.01, 1000000)]
+        public decimal Price { get; set; }
+
+        [Range(0, 24 * 60)]
+        public int BufferBefore { get; set; }
+
+        [Range(0, 24 * 60)]
+        public int BufferAfter { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public bool IsEdit => Id.HasValue;
+    }
+}
---
+++ SimplyAppoint.Models/ViewModels/Services/ServicesIndexVM.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimplyAppoint.Models.ViewModels
+{
+    public class ServicesIndexVM
+    {
+        public List<ServiceRowVM> Services { get; set; } = new();
+
+        public int Total { get; set; }
+        public int ActiveCount { get; set; }
+        public int InactiveCount => Total - ActiveCount;
+
+        public int AvgDuration { get; set; }
+        public decimal AvgPrice { get; set; }
+
+        public decimal MinPrice { get; set; }
+        public decimal MaxPrice { get; set; }
+
+        public int MinDuration { get; set; }
+        public int MaxDuration { get; set; }
+
+        public string? MostExpensiveName { get; set; }
+        public decimal MostExpensivePrice { get; set; }
+
+        public string? LongestName { get; set; }
+        public int LongestDuration { get; set; }
+
+        public int ActivePercentage { get; set; }
+
+        public class ServiceRowVM
+        {
+            public int Id { get; set; }
+            public string Name { get; set; } = "";
+            public int Duration { get; set; }
+            public decimal Price { get; set; }
+            public int BufferBefore { get; set; }
+            public int BufferAfter { get; set; }
+            public bool Active { get; set; }
+        }
+    }
+}
